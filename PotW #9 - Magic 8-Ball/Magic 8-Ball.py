17a92e89d89c7a3d29ee3e73022039507dc899a8
Rick Deckard <118646342+1809031104050311011804@users.noreply.github.com>
2023-01-16 20:04:17 -0500

    Update Magic 8-Ball.py

--- PotW #9 - Magic 8-Ball/Magic 8-Ball.py
+++ PotW #9 - Magic 8-Ball/Magic 8-Ball.py
@@ -13,26 +13,26 @@ def clear():
 def magic_8_ball_responses():
     global responses
 
-    responses = [  "It is certain.",
-                        "It is decidedly so.",
-                        "Without a doubt.",
-                        "Yes, definitely.",
-                        "You may rely on it.",
-                        "As I see it, yes.",
-                        "Most likely.",
-                        "Outlook good.",
-                        "Yes.",
-                        "Signs point to yes.",
-                        "Reply hazy, try again.",
-                        "Ask again later.",
-                        "Better not tell you now.",
-                        "Cannot predict now.",
-                        "Concentrate and ask again.",
-                        "Don't count on it.",
-                        "My reply is no.",
-                        "My sources say no.",
-                        "Outlook not so good.",
-                        "Very doubtful."]
+    responses = [   "It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes, definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful."                ]
 
     clear()
     user_shake()
